ee0b2911aacc66ae589bec08bd9deaa7bfeda688
vinhnguyen <nhquocvinh@gmail.com>
2018-03-05 01:32:10 +0700

    test format ts type

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,13 @@ class App extends React.Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo"/>
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
+          To get started, edit
+          <code>src/App.tsx</code>
+          and save to reload.
         </p>
       </div>
     );
